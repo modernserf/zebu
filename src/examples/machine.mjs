96ddc1b3bff0da92b1f033d93de0d02038ab7ebf
Justin Falcone <justin@justinfalcone.com>
2019-01-04 00:24:01 -0500

    update machine grammar

--- src/examples/machine.mjs
+++ src/examples/machine.mjs
@@ -22,7 +22,14 @@ const inCollection = (x, xs) => {
   return x in xs
 }
 
-const comp = {
+const opMap = {
+  '+': (l, r) => l + r,
+  '-': (l, r) => l - r,
+  '*': (l, r) => l * r,
+  '/': (l, r) => l / r,
+  '%': (l, r) => l % r,
+  '|': (l, r) => l || r,
+  '&': (l, r) => l && r,
   '==': eq,
   '!=': (l, r) => !eq(l, r),
   '>': (l, r) => l > r,
@@ -34,17 +41,8 @@ const comp = {
   is: (l, r) => l === r,
   is_not: (l, r) => l !== r,
 }
+const ops = (l, op, r) => (scope) => opMap[op](l(scope), r(scope))
 
-const op = {
-  '+': (l, r) => l + r,
-  '-': (l, r) => l - r,
-  '*': (l, r) => l * r,
-  '/': (l, r) => l / r,
-  '%': (l, r) => l % r,
-}
-const shiftOps = (key, r) => (l, scope) => op[key](l(scope), r(scope))
-const reduceOps = (l, rs) => (scope) => rs.reduce((l, f) => f(l, scope), l(scope))
-const list = (h, t) => [h, ...t]
 const toObj = (scope) => (obj, [k, f]) => { obj[k] = f(scope); return obj }
 
 const joinThunks = (xs, scope) => (store) => xs.forEach((x) => x(scope)(store))
@@ -141,8 +139,8 @@ export const machine = lang`
     Program = Declaration+ Rule+ ${(ds, rs) => compile([...ds, ...rs])}
     # TODO: track usage of tagged bindings as states/events
     #       to match top-level declarations
-    Declaration = ~"states" ~"=" TagBind ("|" TagBind)*
-                | ~"actions" ~"=" TagBind ("|" TagBind)*
+    Declaration = ~"states" ~"=" TagBind / "|"
+                | ~"actions" ~"=" TagBind / "|"
                 # TODO: "ensure" pattern?
                 # TODO: handle sub-machine as state
                 | ~"start" ~"=" TagExpr ${(value) => ['start', value]}
@@ -151,52 +149,20 @@ export const machine = lang`
          | TagExpr ~"extends" TagBind
            ${(dest, src) => ['extends', (state) => fmap(dest, src({}, state))]}
 
-    # events are ({ State, Action, Scope }) => Maybe Scope
-    # TODO: events as [stateType, actionType, ({state, action, scope}) => Maybe Scope]
-    #       for indexing / type checking
-    OrEvent   = CondEvent (~"|" CondEvent)* 
-                ${(h, t) => (params) => firstM([h, ...t].map(params))}
-    CondEvent = ~"(" OrEvent ")"
-              | State ~"@" Event (~"if" EventCond)?
-                ${(s, e, c = okEvent) => (params) => foldEvents(params, [s, e, c])}
-
-    State     = TagBind ${(f) => ({ state, scope }) => f(scope, state)}
-
-    Event     = TagBind ${(f) => ({ action, scope }) => f(scope, action)}
-                # TODO: set up subscriptions on here?
-              | "after" %number ("s" | "ms")
-              | ("enter" | "exit") (State | Cond)
-
-    EventCond = Cond ${(cond) => ({ scope }) => cond(scope) ? ok(scope) : none}
-
-    # cond are (scope) => bool
-    OrCond    = AndCond (~"|" AndCond)*
-                ${(l, rs) => (scope) => rs.reduce((acc, r) => acc || r(scope), l(scope))}
-    AndCond   = Cond (~"&" Cond)* 
-                ${(l, rs) => (scope) => rs.reduce((acc, r) => acc && r(scope), l(scope))}
-    Cond      = ~"(" OrCond ")"
-              | %function
-              | Expr CondOp Expr ${(l, op, r) => (scope) => comp[op](l(scope), r(scope))}
-              | Expr
-    # NOTE: these use _python_ semantics! 
-    CondOp    = "==" | "!=" | ">=" | ">" | "<=" | "<" 
-              | "in" | "not" "in" ${() => 'not_in'} 
-              | "is" | "is" "not" ${() => 'is_not'}
-    
     # effects are (scope) => Maybe [nextState?, (store) => {}]
     # TODO: [stateType?, [actionTypes], (scope) => Maybe [nextState?, (store) => {}]]
     #       for indexing / type checikng
-    OrEffect    = CondEffect (~"|" CondEffect)*
-                  ${(h, t) => (scope) => firstM([h, ...t].map((f) => f(scope)))}
-    CondEffect  = AndEffect ~"if" Cond 
+    OrEffect    = CondEffect / "|"
+                  ${(xs) => (scope) => firstM(xs.map((f) => f(scope)))}
+    CondEffect  = AndEffect ~"if" Cond
                   ${(f, cond) => (scope) => cond(scope) ? ok(f(scope)) : none}
                 | AndEffect ${(f) => (scope) => ok(f(scope))}
     AndEffect   = NextState (~"&" Effect)*
                   ${(f, xs) => (scope) => [f(scope), joinThunks(xs, scope)]}
-                | Effect (~"&" Effect)*
-                  ${(h, t) => (scope) => [null, joinThunks([h, ...t], scope)]}
+                | Effect / "&"
+                  ${(xs) => (scope) => [null, joinThunks(xs, scope)]}
     NextState   = TagExpr ${(getExpr) => (scope) => getExpr(scope)}
-    Effect      = ~"(" OrEffect ")"
+    Effect      = ["(" OrEffect ")"]
                 | ~"dispatch" TagExpr 
                   ${(getExpr) => (scope) => (store) => store.dispatch(getExpr(scope))}
                 | ~"await" %function 
@@ -204,38 +170,71 @@ export const machine = lang`
                 | ~"exec" %function
                   ${(gen) => (scope) => (store) => runGenerator(store, gen(scope))}
 
+    # events are ({ State, Action, Scope }) => Maybe Scope
+    # TODO: events as [stateType, actionType, ({state, action, scope}) => Maybe Scope]
+    #       for indexing / type checking
+    OrEvent   = CondEvent / "|"
+                ${(xs) => (params) => firstM(xs.map(params))}
+    CondEvent = ["(" OrEvent ")"]
+              | State ~"@" Event (~"if" EventCond)?
+                ${(s, e, c = okEvent) => (params) => foldEvents(params, [s, e, c])}
+
+    Event     = TagBind ${(f) => ({ action, scope }) => f(scope, action)}
+                # TODO: set up subscriptions on here?
+              | "after" %number ("s" | "ms")
+              | ("enter" | "exit") (State | Cond)
+    State     = TagBind ${(f) => ({ state, scope }) => f(scope, state)}
+
+    EventCond = Cond ${(cond) => ({ scope }) => cond(scope) ? ok(scope) : none}
+
+    # cond are (scope) => bool
+    OrCond    = < . ~"|" AndCond > ${ops}
+              | AndCond
+    AndCond   = < . ~"&" Cond > ${ops}
+              | Cond
+    Cond      = ["(" OrCond ")"]
+              | %function
+              | Expr CondOp Expr ${ops}
+              | Expr
+    # NOTE: these use _python_ semantics! 
+    CondOp    = "==" | "!=" | ">=" | ">" | "<=" | "<" 
+              | "in" | "not" "in" ${() => 'not_in'} 
+              | "is" | "is" "not" ${() => 'is_not'}
+
     # bindings are (Scope, Value) => Maybe Scope
     TagBind  = ~"#" %ident BindPair* ${tagBind}
              | Binding
-    Binding  = ~"{" (Pair (~"," BindPair)* ","? ${list})? "}"
+    BindPair = (%ident | %string) ~":" Binding ${(key, f) => [key, f]}
+             | %ident ${(key) => [key, (scope, value) => ok({ ...scope, [key]: value })]}
+    Binding  = ["(" TagBind ")"]
+             | ["{" (BindPair / ",")? "}"]
                ${(xs) => (initSc, value) => foldM((sc, [key, f]) => f(sc, value[key]), initSc, xs)}
-             | ~"[" (Binding (~"," Binding)* ","? ${list})? "]"
+             | ["[" (Binding / ",")? "]"]
                ${(xs) => (initSc, value) => foldM((sc, f, i) => f(sc, value[i]), initSc, xs)}
-             | ~"(" TagBind ")"
              | "_"      ${() => (scope) => ok(scope)}
              | LitValue ${(match) => (scope, value) => match === value ? ok(scope) : none}
              | %ident   ${(key) => (scope, value) => ok({ ...scope, [key]: value })}
-    BindPair = (%ident | %string) ~":" Binding ${(key, f) => [key, f]}
-             | %ident ${(key) => [key, (scope, value) => ok({ ...scope, [key]: value })]}
 
     # exprs are fn called with object containing current scope as arg
     TagExpr  = ~"#" %ident ExprPair*
                ${(type, xs) => (scope) => xs.reduce(toObj(scope), { type })}
              | Expr
-    Expr     = MulExpr (("+" | "-") MulExpr ${shiftOps})* ${reduceOps}
-    MulExpr  = PowExpr (("*" | "/" | "%") PowExpr ${shiftOps})* ${reduceOps}
-    PowExpr  = BaseExpr ~"**" PowExpr ${(l, r) => (scope) => l(scope) ** r(scope)}
+    ExprPair = (%ident | %string) ~":" Expr ${(key, fn) => [key, fn]}
+             | %ident ${(key) => [key, (scope) => scope[key]]}
+    Expr     = < . ("+" | "-") MulExpr > ${ops}
+             | MulExpr
+    MulExpr  = < . ("*" | "/" | "%") PowExpr > ${ops}
+             | PowExpr
+    PowExpr  = < BaseExpr "**" . > ${ops}
              | BaseExpr
-    BaseExpr = ~"(" TagExpr ")"
-             | ~"[" (Expr (~"," Expr)* ","? ${list})? "]" 
-               ${(xs) => (scope) => xs.map((f) => f(scope))}
-             | ~"{" (ExprPair (~"," ExprPair)* ","? ${list})? "}" 
+    BaseExpr = ["(" TagExpr ")"]
+             | ["{" (ExprPair / ",")? "}"]
                ${(xs) => (scope) => xs.reduce(toObj(scope), {})}
+             | ["[" (Expr / ",")? "]"]
+               ${(xs) => (scope) => xs.map((f) => f(scope))}
              | LitValue    ${(value) => () => value}
              | %ident      ${(ident) => (scope) => scope[ident]}
              | %function
-    ExprPair = (%ident | %string) ~":" Expr ${(key, fn) => [key, fn]}
-             | %ident ${(key) => [key, (scope) => scope[key]]}
 
     LitValue = "true"      ${() => true}
              | "false"     ${() => false}
