3209bf43124c3e750264af267f5f5f9adc94a9be
Justin Falcone <justin@justinfalcone.com>
2019-01-05 15:46:10 -0500

    parser interpolation wip

---
+++ src/examples/micro.mjs
@@ -0,0 +1,25 @@
+import { lang } from '../../index'
+
+const isInt = (x) => x === Math.floor(x)
+
+const time = lang`
+  %number ~":" %number (~":" %number) ${(h, m, s = 0) => {
+    const isValid = h < 24 && m < 60 && s < 60 &&
+      h >= 0 && m >= 0 && s >= 0 &&
+      isInt(h) && isInt(m)
+    if (!isValid) { throw new Error(`invalid time ${h}:${m}:${s}`) }
+    return { hour: h, minute: m, second: s }
+  }}
+`
+
+const date = lang`
+  %number "-" %number "-" %number ${(y, m, d) => {
+    const isValid = m <= 12 && d <= 31 &&
+      m >= 1 && d >= 1 &&
+      isInt(y) && isInt(m) && isInt(d)
+    if (!isValid) { throw new Error(`invalid date ${y}-${m}-${d}`) }
+    return { year: y, month: m, date: d }
+  }}
+`
+
+const dateTime = lang`${date} ${time} ${(date, time) => ({ ...date, ...time })}`
