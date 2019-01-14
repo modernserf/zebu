f3e91374fc17b38ae6f73c2617520690f092b19d
Justin Falcone <justin@justinfalcone.com>
2019-01-14 08:51:50 -0500

    use next in proptypes

--- src/examples/prop-types.mjs
+++ src/examples/prop-types.mjs
@@ -6,22 +6,22 @@ const fromPairs = (pairs) => pairs.reduce(Object.assign, {})
 const ifMultiple = (fn) => (xs) => xs.length === 1 ? xs[0] : fn(xs)
 
 export const propTypes = grammar`
-  Rules     = Rule ** (line | ";")  : ${fromPairs}
-  Rule      = identifier ":" Expr   : ${pair}
+  Rules = Rule ** (line | ";")  : ${fromPairs}
+  Rule  = identifier ":" Expr   : ${pair}
 
-  Pair      = Key ":" Expr          : ${pair}
-  Key       = identifier | value
+  Pair  = Key ":" Expr          : ${pair}
+  Key   = identifier | value
 
-  Expr      = OptExpr ++ "|"        : ${ifMultiple(PropTypes.oneOfType)}
-  OptExpr   = BaseExpr "?"          : ${(type) => type}
-            | BaseExpr              : ${(type) => type.isRequired}
-  BaseExpr  = #( Expr )
-            | #{ ":" Expr }         : ${(expr) => PropTypes.objectOf(expr)}
-            | #{ Pair ** "," }      : ${(pairs) => PropTypes.shape(fromPairs(pairs))}
-            | #[ Expr ]             : ${PropTypes.arrayOf}
-            | "instanceof" value    : ${PropTypes.instanceOf}
-            | identifier            : ${(id) => PropTypes[id]}
-            | value                 : ${(value) => typeof value === 'function' ? value : PropTypes.oneOf([value])}
+  Expr  = next ++ "|"           : ${ifMultiple(PropTypes.oneOfType)}
+  ...   = next "?"              : ${(type) => type}
+        | next                  : ${(type) => type.isRequired}
+  ...   = #( Expr )
+        | #{ ":" Expr }         : ${(expr) => PropTypes.objectOf(expr)}
+        | #{ Pair ** "," }      : ${(pairs) => PropTypes.shape(fromPairs(pairs))}
+        | #[ Expr ]             : ${PropTypes.arrayOf}
+        | "instanceof" value    : ${PropTypes.instanceOf}
+        | identifier            : ${(id) => PropTypes[id]}
+        | value                 : ${(value) => typeof value === 'function' ? value : PropTypes.oneOf([value])}
 `
 
 export function test_react_prop_types (expect) {
